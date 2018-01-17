021a34c230aae150f57befd767691f5746d6953b
Mathias Buus <mathiasbuus@gmail.com>
2018-01-18 00:58:40 +0100

    support only, skip

--- index.js
+++ index.js
@@ -28,89 +28,94 @@ test.createController = function () {
 
 module.exports = test
 
+test.only = (name, fn) => tape.only(name, t => runner(t, fn))
+test.skip = (name, fn) => {}
+
 function test (name, fn) {
-  tape(name, function (t) {
-    if (!mn.started) {
-      mn.on('start', ready)
-      mn.start()
-    } else {
-      ready()
-    }
+  tape(name, t => runner(t, fn))
+}
 
-    function ready () {
-      t.run = run
+function runner (t, fn) {
+  if (!mn.started) {
+    mn.on('start', ready)
+    mn.start()
+  } else {
+    ready()
+  }
 
-      var missing = pending.length
-      if (!missing) fn(t)
+  function ready () {
+    t.run = run
 
-      pending.forEach(function (proc) {
-        proc.on('close', function () {
-          if (!--missing) {
-            pending = []
-            fn(t)
-          }
-        })
-        proc.kill()
-      })
-    }
+    var missing = pending.length
+    if (!missing) fn(t)
 
-    function run (host, src) {
-      var hostFilename = require.resolve('mininet/host')
-
-      if (typeof src === 'function') src = ';(' + src.toString() + ')()'
-
-      var proc = host.spawnNode(`
-        var EventEmitter = require('events').EventEmitter
-        var host = require('${hostFilename}')
-        var target = {}
-        var ip = '${host.ip}'
-        var mac = '${host.mac}'
-        var __dirname = '${parentDirname}'
-        var __filename = '${parentFilename}'
-
-        global.t = new Proxy({}, {
-          get: function (target, name) {
-            return function (...args) {
-              host.send('test', {name, args})
-            }
-          }
-        })
-
-        var handler =  
-        vm.runInNewContext(
-          ${JSON.stringify(src)},
-          new Proxy(target, {
-            get: function (target, name) {
-              if (global.hasOwnProperty(name)) return global[name]
-              if (target.hasOwnProperty(name)) return target[name]
-              if (!/^h\\d$/.test(name)) return
-              var e = target[name] = createEmitter(name)
-              return e
-            }
-          }),
-          {filename: '[${host.id}-test]'}
-        )
-
-        function createEmitter (id) {
-          var e = new EventEmitter()
-          e.id = id
-          e.emit = function (...args) {
-            EventEmitter.prototype.emit.apply(this, arguments)
-            host.broadcast(id + ':emit', args)
-            return true
-          }
-          host.on('message:' + id + ':emit', function (args) {
-            EventEmitter.prototype.emit.apply(e, args)
-          })
-          return e
+    pending.forEach(function (proc) {
+      proc.on('close', function () {
+        if (!--missing) {
+          pending = []
+          fn(t)
         }
-      `)
+      })
+      proc.kill()
+    })
+  }
 
-      proc.on('message:test', function (data) {
-        t[data.name].apply(t, data.args)
+  function run (host, src) {
+    var hostFilename = require.resolve('mininet/host')
+
+    if (typeof src === 'function') src = ';(' + src.toString() + ')()'
+
+    var proc = host.spawnNode(`
+      var EventEmitter = require('events').EventEmitter
+      var host = require('${hostFilename}')
+      var target = {}
+      var ip = '${host.ip}'
+      var mac = '${host.mac}'
+      var __dirname = '${parentDirname}'
+      var __filename = '${parentFilename}'
+
+      global.t = new Proxy({}, {
+        get: function (target, name) {
+          return function (...args) {
+            host.send('test', {name, args})
+          }
+        }
       })
 
-      return proc
-    }
-  })
+      var handler =  
+      vm.runInNewContext(
+        ${JSON.stringify(src)},
+        new Proxy(target, {
+          get: function (target, name) {
+            if (global.hasOwnProperty(name)) return global[name]
+            if (target.hasOwnProperty(name)) return target[name]
+            if (!/^h\\d$/.test(name)) return
+            var e = target[name] = createEmitter(name)
+            return e
+          }
+        }),
+        {filename: '[${host.id}-test]'}
+      )
+
+      function createEmitter (id) {
+        var e = new EventEmitter()
+        e.id = id
+        e.emit = function (...args) {
+          EventEmitter.prototype.emit.apply(this, arguments)
+          host.broadcast(id + ':emit', args)
+          return true
+        }
+        host.on('message:' + id + ':emit', function (args) {
+          EventEmitter.prototype.emit.apply(e, args)
+        })
+        return e
+      }
+    `)
+
+    proc.on('message:test', function (data) {
+      t[data.name].apply(t, data.args)
+    })
+
+    return proc
+  }
 }
